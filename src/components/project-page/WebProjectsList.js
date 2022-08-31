5b3eb53d378a76de51341cf0761adbf17b6cbee0
Md Sahil <Mohdssahil1@gmail.com>
2022-09-01 00:26:17 +0530

    Enhanced

---
+++ src/components/project-page/WebProjectsList.js
@@ -0,0 +1,9 @@
+import Plone from '../../images/Plone.png'
+
+export const webProjectsList = [
+    {
+        "name": "Undo Controlpanel",
+        "logo": Plone,
+        "usedTechnologies": ["HTML, CSS, Javascript", "Semantic UI", "React, Python, Plone, ZopeDB"],
+    }
+]
