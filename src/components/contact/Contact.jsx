c3439cab13d461dca5dc089d32a4cffee8c8456f
Md Sahil <Mohdssahil1@gmail.com>
2022-09-04 22:49:08 +0530

    Added new pages

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,28 +3,29 @@ import React from "react";
 class Contact extends React.Component {
     render() {
         return (
-            <div className="custom-scrolling-container border basis-full h-screen overflow-x-auto flex justify-center items-center">
-                <div className="border w-3/5 h-auto text-slate-500 space-y-5">
-                    <div className="text-center flex flex-col justify-center items-center">
+            <div className=" custom-scrolling-container basis-full sm:static absolute h-screen w-screen overflow-x-auto flex justify-center items-center">
+                <div className=" w-10/12 sm:w-3/5 h-auto text-slate-500 space-y-5">
+                    <div className=" text-center flex flex-col justify-center items-center">
                         <h3 className=" text-3xl font-bold">Let me know the reason</h3>
                         <h4 className=" text-lg font-semibold">why do you want to contact me</h4>
                     </div>
-                    <form className="space-y-5 flex flex-col justify-center items-center">
-                        <div className=" space-x-3 w-5/12 flex justify-between">
+                    <form className=" w-full space-y-5 flex flex-col justify-center items-center">
+                        <div className=" space-x-3 w-4/5 lg:w-5/12 flex justify-between">
                             <label htmlFor="name">Name</label>
-                            <input id="name" className="w-72 h-10 rounded-3xl text-center" type="text" />
+                            <input id="name" className="w-4/5 h-10 rounded-3xl text-center" type="text" />
                         </div>
-                        <div className=" space-x-3 w-5/12 flex justify-between">
+                        <div className=" space-x-3 w-4/5 lg:w-5/12 flex justify-between">
                             <label htmlFor="email">Email</label>
-                            <input id="email" className="w-72 h-10 rounded-3xl text-center" type="email" />
+                            <input id="email" className="w-4/5 h-10 rounded-3xl text-center" type="email" />
                         </div>
-                        <div className=" space-x-3 w-5/12 flex">
+                        <div className=" w-4/5 lg:w-5/12 space-x-3 flex">
                             <label htmlFor="message">Message</label>
-                            <textarea id="message" className="w-72 rounded-xl h-32 " type="text" />
+                            <textarea id="message" className=" w-4/5 rounded-xl h-32 " type="text" />
                         </div>
                         <div className="w-fit space-x-2 pl-12 flex">
-                            {/* <input type="button" className=" border w-32 h-9 rounded-3xl" value="Submit" /> */}
-                            <button className=" border border-blue-500 w-32 h-9 rounded-3xl bg-blue-500 hover:bg-slate-900 hover:text-blue-500 text-slate-900">Sumit</button>
+                            <button className="hover:bg-slate-900 text-white border border-slate-700 bg-slate-700  hover:text-slate-700 w-32 h-9 rounded-2xl">
+                                Submit
+                            </button>
                             <button className=" border border-red-700 w-32 h-9 rounded-3xl bg-red-700 text-black hover:bg-slate-900 hover:text-red-700">Cancel</button>
                         </div>
                     </form>
--- src/components/no-page/NoPage.jsx
+++ src/components/no-page/NoPage.jsx
@@ -3,14 +3,23 @@ import React from "react";
 class NoPage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Hey! you got Lost
-                </h1>
-                <p>
-                    We don't have any page with this URL.
-                    Don't Worry Go back to the Home NoPage
-                </p>
+            <div className="custom-scrolling-container w-screen h-screen flex justify-center items-center">
+                <div className=" w-4/6 h-auto  space-y-3">
+                    <h1 className=" text-2xl font-bold text-slate-500 text-center">
+                        Hey! I think you got Lost
+                    </h1>
+                    <div>
+                        <p className="text-xl font-semibold text-center">
+                            We <span className="text-red-900">don't</span> have any page with this URL
+                        </p>
+                        <p className="text-xl font-semibold text-center">
+                            <span className="text-red-900">Don't</span> Worry Go back to the Home page
+                        </p>
+                    </div>
+                    <a href="/" className=" flex justify-center">
+                        <button className=" border border-blue-700 w-32 h-9 rounded-3xl bg-blue-700 text-black hover:bg-slate-900 hover:text-blue-700">Go Back</button>
+                    </a>
+                </div>
             </div>
         )
     }
